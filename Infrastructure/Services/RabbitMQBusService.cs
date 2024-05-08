b093dbe4aae461e979c63d68ebc0a51c7ce4665f
amisuu <liver13@wp.pl>
2024-05-08 17:06:40 +0200

    Added process event method

--- Infrastructure/Services/RabbitMQBusService.cs
+++ Infrastructure/Services/RabbitMQBusService.cs
@@ -7,6 +7,8 @@ using Polly;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using RabbitMQ.Client.Exceptions;
+using System;
+using System.Diagnostics.Tracing;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
@@ -143,8 +145,9 @@ namespace Infrastructure.Services
 
             try
             {
-                //await ProcessEvent(eventName, message);
+                await ProcessEvent(eventName, message);
 
+                _consumerChannel.BasicAck(e.DeliveryTag, multiple: false);
                 isAcknowledged = true;
             }
             catch (Exception ex)
@@ -153,7 +156,10 @@ namespace Infrastructure.Services
             }
             finally
             {
-                //todo
+                if (!isAcknowledged)
+                {
+                    await TryEnqueueMessageAgainAsync(e);
+                }
             }
         }
 
@@ -173,6 +179,52 @@ namespace Infrastructure.Services
             using (var channel = _persistentConnection.CreateModel())
             {
                 channel.QueueBind(queue: _queueName, exchange: _exchangeName, routingKey: eventName);
+            }
+        }
+
+        private async Task ProcessEvent(string eventName, string message)
+        {
+            if (!_subscriptionsManager.HasSubscriptionsForEvent(eventName))
+            {
+                _logger.LogTrace("There are no subscriptions.");
+                return;
+            }
+
+            var subscriptions = _subscriptionsManager.GetHandlersForEvent(eventName);
+            foreach (var subscription in subscriptions)
+            {
+                var handler = _serviceProvider.GetService(subscription.HandlerType);
+                if (handler == null)
+                {
+                    _logger.LogWarning("There are no handlers for the event: {EventName}", eventName);
+                    continue;
+                }
+
+                var eventType = _subscriptionsManager.GetEventTypeByName(eventName);
+
+                var @event = JsonSerializer.Deserialize(message, eventType);
+                var eventHandlerType = typeof(IEventHandler<>).MakeGenericType(eventType);
+                await Task.Yield();
+                await (Task)eventHandlerType.GetMethod(nameof(IEventHandler<Event>.HandleAsync)).Invoke(handler, new object[] { @event });
+            }
+
+            _logger.LogTrace("Processed event {EventName}.", eventName);
+        }
+
+        private async Task TryEnqueueMessageAgainAsync(BasicDeliverEventArgs e)
+        {
+            try
+            {
+                _logger.LogWarning("Adding message to queue again with {Time} seconds delay...", $"{_subscribeRetryTime.TotalSeconds:n1}");
+
+                await Task.Delay(_subscribeRetryTime);
+                _consumerChannel.BasicNack(e.DeliveryTag, false, true);
+
+                _logger.LogTrace("Message added to queue.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error with enqueue message: {Error}.", ex.Message);
             }
         }
 
